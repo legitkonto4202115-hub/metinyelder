6aeca265459a13bc4ab3db118b6857e261a3059d
legitkonto4202115-hub <legitkonto4202115@gmail.com>
2026-08-17 21:18:28 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C='elder-metin-v4-0-2-points-only',A=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{let cp=resp.clone();caches.open(C).then(c=>c.put(e.request,cp)).catch(()=>{});return resp;}))));
